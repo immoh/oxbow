3ddf741ad006b1be813c7463a148d33391f96ee6
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-02-25 09:48:43 +0200

    Add test project to dev classpath

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-midje "3.1.3"]]
-  :profiles {:dev {:dependencies [[midje "1.6.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.0"]]
+                   :source-paths ["test-resources/"]}})
