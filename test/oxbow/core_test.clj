da235327e4601b425601812ed0f24dde40ef434f
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-03-06 15:31:29 +0200

    Reorganize core test file

--- test/oxbow/core_test.clj
+++ test/oxbow/core_test.clj
@@ -4,29 +4,38 @@
 
 (def check-test-project (memoize (fn [] (core/check "test-resources/test_project"))))
 
-;; :dead-ns
+(facts "About :dead-ns checker"
 
-(fact "Namespaces that are not referenced from anywhere are reported as dead"
-  (check-test-project) => (contains {:type :dead-ns :name 'test-project.deps.b}))
+  (fact "Namespaces that are not referenced from anywhere are reported as dead"
+    (check-test-project) => (contains {:type :dead-ns
+                                       :name 'test-project.deps.b}))
 
-(fact "Namespaces imported with :require  are not reported as dead"
-  (check-test-project) =not=> (contains {:type :dead-ns :name 'test-project.deps.a}))
+  (fact "Namespaces imported with :require  are not reported as dead"
+    (check-test-project) =not=> (contains {:type :dead-ns
+                                           :name 'test-project.deps.a}))
 
-(fact "Namespaces imported with :use are not reported as dead"
-  (check-test-project) =not=> (contains {:type :dead-ns :name 'test-project.deps.c}))
+  (fact "Namespaces imported with :use are not reported as dead"
+    (check-test-project) =not=> (contains {:type :dead-ns
+                                           :name 'test-project.deps.c})))
 
-;; :unused-require
 
-(fact "Unused require is reported"
-  (check-test-project) => (contains {:type :unused-require :ns 'test-project.core :spec '[test-project.deps.d :as d]}))
+(facts "About :unused-ns checker"
 
-(fact "Used require is not reported"
-  (check-test-project) =not=> (contains {:type :unused-require :ns 'test-project.core :spec '[test-project.deps.a :as a]}))
+  (fact "Unused require is reported"
+    (check-test-project) => (contains {:type :unused-require
+                                       :ns   'test-project.core
+                                       :spec '[test-project.deps.d :as d]}))
 
-;; unused-require-refer-symbols
+  (fact "Used require is not reported"
+    (check-test-project) =not=> (contains {:type :unused-require
+                                           :ns   'test-project.core
+                                           :spec '[test-project.deps.a :as a]})))
 
-(fact "Unused referred symbols from require are reported"
- (check-test-project) => (contains {:type :unused-require-refer-symbols
-                                    :ns   'test-project.core
-                                    :spec '[test-project.deps.e :refer [my-function other-function]]
-                                    :symbols '(my-function)}))
+
+(facts "About :unused-require-refer-symbols checker"
+
+  (fact "Unused referred symbols from require are reported"
+    (check-test-project) => (contains {:type    :unused-require-refer-symbols
+                                       :ns      'test-project.core
+                                       :spec    '[test-project.deps.e :refer [my-function other-function]]
+                                       :symbols '(my-function)})))
