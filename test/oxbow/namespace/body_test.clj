c09d52ca57efad0f047ade87f086879d550b3d41
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-03-13 15:28:38 +0200

    Add test for oxbow.namespace.body

---
+++ test/oxbow/namespace/body_test.clj
@@ -0,0 +1,18 @@
+(ns oxbow.namespace.body-test
+  (:use midje.sweet)
+  (:require [oxbow.namespace.body :as ns-body]))
+
+(fact "Resolves referred symbols"
+  (binding [*ns* *ns*]
+    (ns test (:require [clojure.set :refer [difference]]))
+    (ns-body/analyze ['(difference #{1 2} #{1})])) => {:resolved-symbols {'difference 'clojure.set}})
+
+(fact "Doesn't resolve namespace qualified symbols"
+  (binding [*ns* *ns*]
+    (ns test (:require clojure.set))
+    (ns-body/analyze ['(clojure.set/difference #{1 2} #{1})]) => {:resolved-symbols {}}))
+
+(fact "Doesn't resolve locals"
+  (binding [*ns* *ns*]
+    (ns test (:require [clojure.set :refer [difference]]))
+    (ns-body/analyze ['(let [difference 1] (inc difference))]) => {:resolved-symbols {}}))
