db4535d5e9ff7794241a5a4711338a5221d7a785
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-04-02 15:08:59 +0300

    Analyze macros too

--- src/oxbow/namespace/body.clj
+++ src/oxbow/namespace/body.clj
@@ -12,16 +12,15 @@
         (swap! res-atom assoc sym resolved)))))  
 
 (defn- symbols-to-vars [form]
-  (when-not (= 'defmacro (first form))
-    (let [resolved-symbols (atom {})]
-      (walk/walk-exprs symbol?
-                       (fn [sym] (resolve-and-store resolved-symbols sym) sym)
-                       (fn [first-of-form]
-                         (when (symbol? first-of-form)
-                           (resolve-and-store resolved-symbols first-of-form))
-                         false)
-                       form)
-      @resolved-symbols)))
+  (let [resolved-symbols (atom {})]
+    (walk/walk-exprs symbol?
+                     (fn [sym] (resolve-and-store resolved-symbols sym) sym)
+                     (fn [first-of-form]
+                       (when (symbol? first-of-form)
+                         (resolve-and-store resolved-symbols first-of-form))
+                       false)
+                     form)
+    @resolved-symbols))
 
 (defn analyze [forms]
   {:symbols-to-vars (apply merge (map symbols-to-vars forms))})
