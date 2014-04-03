75d9b6940d16f8a0b0b2203bc76c80d35652f330
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-04-03 12:42:24 +0300

    Implement checker for unused-require correctly

--- src/oxbow/checkers/unused_require.clj
+++ src/oxbow/checkers/unused_require.clj
@@ -1,24 +1,42 @@
-(ns oxbow.checkers.unused-require)
+(ns oxbow.checkers.unused-require
+  (:require clojure.set))
 
-(defn- parse-namespaces-from-symbols [forms]
-  (->> forms
-       (mapcat (partial tree-seq sequential? identity))
-       (filter symbol?)
-       (keep namespace)
-       (set)))
+(defmulti find-unused-refer (fn [{:keys [refer]} used-symbols] refer))
 
-(defn is-require-used? [used-namespaces {:keys [ns as]}]
-  (used-namespaces (str (or as ns))))
+(defmethod find-unused-refer nil [& _] nil)
 
-(defn format-result [ns {:keys [spec]}]
-  {:type :unused-require :ns ns :spec spec})
+(defmethod find-unused-refer :all [{required-ns :ns} used-symbols]
+  (when-not (seq (get used-symbols required-ns))
+    {:type :unused-require-refer-all}))
 
-(defn- find-unused-requires [{:keys [ns requires forms]}]
-  (let [used-namespaces (parse-namespaces-from-symbols forms)]
-    (some->> requires
-             (remove (partial is-require-used? used-namespaces))
-             seq
-             (map (partial format-result ns)))))
+(defmethod find-unused-refer :default [{required-ns :ns refer :refer} used-symbols]
+  (when-let [unused (clojure.set/difference (set refer) (get used-symbols required-ns))]
+    {:type :unused-require-refer-symbols :symbols (seq unused)}))
+
+(defn- find-unused-require [{required-ns :ns} used-nses]
+  (when-not (contains? used-nses required-ns)
+    {:type :unused-require}))
+
+(defn- var->ns-name [v]
+  (-> v .-ns ns-name))
+
+(defn- invert [m]
+  (apply merge-with clojure.set/union (map (fn [[k v]] {v #{k}}) m)))
+
+(defn- ns-to-unqualified-symbols [symbols-to-vars]
+  (invert (keep (fn [[k v]] (when-not (namespace k) [k (var->ns-name v)])) symbols-to-vars)))
+
+(defn- used-nses [symbols-to-vars]
+  (set (keep var->ns-name (vals symbols-to-vars))))
+
+(defn- check-ns-require [ns used-symbols used-nses require]
+  (when-let [result (or
+                      (find-unused-refer require used-symbols)
+                      (find-unused-require require used-nses))]
+    (assoc result :ns ns :spec (:spec require))))
+
+(defn- check-ns [{:keys [ns symbols-to-vars requires]}]
+  (keep (partial check-ns-require ns (ns-to-unqualified-symbols symbols-to-vars) (used-nses symbols-to-vars)) requires))
 
 (defn check [ns-infos]
-  (mapcat find-unused-requires ns-infos))
+  (mapcat check-ns ns-infos))
--- src/oxbow/core.clj
+++ src/oxbow/core.clj
@@ -1,10 +1,9 @@
 (ns oxbow.core
-  (:require [oxbow.checkers dead-ns unused-require unused-require-refer]
+  (:require [oxbow.checkers dead-ns unused-require]
             [oxbow.namespace.file :as ns-file]))
 
 (def checkers [oxbow.checkers.dead-ns/check
-               oxbow.checkers.unused-require/check
-               oxbow.checkers.unused-require-refer/check])
+               oxbow.checkers.unused-require/check])
 
 (defn check [path]
   (let [ns-infos (map ns-file/analyze (ns-file/find-recursively path))]
