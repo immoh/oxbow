f0b9bab28732ed8c031c404134b93d99f330d61a
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-08-29 09:34:04 +0300

    Bump tools.reader version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.6"]
+                 [org.clojure/tools.reader "0.8.7"]
                  [riddley "0.1.8-SNAPSHOT"]]
   :plugins [[lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
