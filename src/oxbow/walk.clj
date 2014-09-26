5e83b983dd9081eff38aac3f54efea9f9628c1ba
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-09-26 09:55:45 +0300

    Working walker

--- src/oxbow/walk.clj
+++ src/oxbow/walk.clj
@@ -1,16 +1,5 @@
 (ns oxbow.walk)
-
-;; locals
-;; - def
-;; - try-catch
-;; - reify
-;; - deftype
-
-
-;; macroexpand-1 doesn't preserve meta
-
 ;; local analysis for empty binding forms
-
 
 (def ^{:dynamic true :private true} *locals* (atom nil))
 
@@ -31,7 +20,6 @@
 
 (defn register-locals [& x]
   (swap! *locals* merge (zipmap x (repeatedly unique-id))))
-
 
 (defmulti handle-special-form (fn [_ x] (first x)))
 
@@ -63,38 +51,61 @@
         (dorun (map f body))))))
 
 (defmethod handle-special-form 'def [f x]
-  (let [[def' sym] x
-        init (last x)]
+  (let [[def' sym & remainder] x]
     (f def')
     (f sym)
     (register-locals sym)
-    (f init)))
+    (dorun (map f remainder))))
 
+(defn- split-try-catch-finally [x]
+  (partition-by #(when (seq? %)
+                  (#{'catch 'finally} (first %)))
+                x))
 
-(defn walk [f x]
-  #_(println "walk:" x)
-  (let [x' (if (seq? x) (macroexpand-1 x) x)]
-
-    (when-not (identical? x x')
-      ; call for first symbol before macroexpansion
-      (f (first x)))
-
-    (f x')
-
-    (cond
-      (and (seq? x') (get (methods handle-special-form) (first x')))
+(defmethod handle-special-form 'try [f x]
+  (let [[try-exprs catch-clauses finally-clauses] (split-try-catch-finally x)]
+    (dorun (map f try-exprs))
+    (doseq [[catch' e-class e-sym & body] catch-clauses]
+      (dorun (map f [catch' e-class e-sym]))
       (with-lexical-context
-        (handle-special-form (partial walk f) x'))
+        (register-locals e-sym)
+        (dorun (map f body))))
+    (dorun (map f finally-clauses))))
 
-      (coll? x')
-      (dorun (map (partial walk f) x')))))
+(defn- handle-method [f [method-name args & body]]
+  (dorun (map f [method-name args]))
+  (with-lexical-context
+    (apply register-locals args)
+    (dorun (map f body))))
 
-(println "---------------")
-(println)
+(defmethod handle-special-form 'deftype* [f x]
+  (let [[deftype' type resolved-type fields implements' interfaces & methods] x]
+    (dorun (map f [deftype' type resolved-type fields]))
+    (apply register-locals fields)
+    (dorun (map f [implements' interfaces]))
+    (dorun (map (partial handle-method f) methods))))
+
+(defmethod handle-special-form 'reify* [f x]
+  (let [[reify' classes & methods] x]
+    (dorun (map f [reify' classes]))
+    (dorun (map (partial handle-method f) methods))))
+
+(defmethod handle-special-form :default [f x]
+  (dorun (map f x)))
+
+(defn walk! [f x]
+  (let [x' (if (seq? x) (macroexpand-1 x) x)]
+    (when-not (identical? x x')
+      (f (first x)))
+    (f x')
+    (cond
+      (and (seq? x') (special-form? (first x'))) (with-lexical-context
+                                                   (handle-special-form (partial walk f) x'))
+      (coll? x')     (dorun (map (partial walk f) x')))))
 
 #_(walk prn '(defn foo [x] 1))
 
-(defn print-env [x]
+#_(defn print-env [x]
   (prn x)
   (prn (locals))
   (println))
@@ -108,5 +119,24 @@
 #_(walk identity '(fn moi [x] 1))
 
 
-(walk print-env '(def x "this is 5" 5))
+#_(walk print-env '(def x "this is 5" 5))
+
+#_(walk print-env '(try
+                  1
+                  (+ 1 1)
+                  (catch RuntimeException r
+                    2
+                    (+ 2 2))
+                  (catch Exception e
+                    3
+                    (+ 3 3))
+                  (finally
+                    4
+                    (+ 4 4))))
+
+#_(walk print-env '(deftype Foo [x] Object (toString [this] "moi")))
+
+#_(walk print-env '(reify java.util.Date (toString [foo] "moi")))
+
+
 
