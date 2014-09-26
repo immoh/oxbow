c119fde0221ac6c74808310d118990c2c8915a36
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-09-26 21:10:59 +0300

    finalize walk

--- src/oxbow/walk.clj
+++ src/oxbow/walk.clj
@@ -1,5 +1,4 @@
 (ns oxbow.walk)
-;; local analysis for empty binding forms
 
 (def ^{:dynamic true :private true} *locals* (atom nil))
 
@@ -8,22 +7,22 @@
     @*locals*))
 
 (let [a (atom 0)]
-  (defn unique-id []
+  (defn- unique-id []
     (swap! a inc)))
 
-(defn with-lexical-context* [f]
+(defn- with-lexical-context* [f]
   (binding [*locals* (atom (locals))]
     (f)))
 
-(defmacro with-lexical-context [& body]
+(defmacro ^:private with-lexical-context [& body]
   `(with-lexical-context* (fn [] ~@body)))
 
-(defn register-locals [& x]
+(defn- register-locals [& x]
   (swap! *locals* merge (zipmap x (repeatedly unique-id))))
 
-(defmulti handle-special-form (fn [_ x] (first x)))
+(defmulti ^:private handle-special-form (fn [_ x] (first x)))
 
-(defn handle-let-and-loop [f x]
+(defn- handle-let-and-loop [f x]
   (let [[sym bindings & body] x]
     (f sym)
     (doseq [[k v] (partition 2 bindings)]
@@ -48,7 +47,8 @@
       (dorun (map f args))
       (with-lexical-context
         (apply register-locals args)
-        (dorun (map f body))))))
+        (dorun (map f body))
+        (f ::end-of-binding-form)))))
 
 (defmethod handle-special-form 'def [f x]
   (let [[def' sym & remainder] x]
@@ -69,14 +69,16 @@
       (dorun (map f [catch' e-class e-sym]))
       (with-lexical-context
         (register-locals e-sym)
-        (dorun (map f body))))
+        (dorun (map f body))
+        (f ::end-of-binding-form)))
     (dorun (map f finally-clauses))))
 
 (defn- handle-method [f [method-name args & body]]
   (dorun (map f [method-name args]))
   (with-lexical-context
     (apply register-locals args)
-    (dorun (map f body))))
+    (dorun (map f body))
+    (f ::end-of-binding-form)))
 
 (defmethod handle-special-form 'deftype* [f x]
   (let [[deftype' type resolved-type fields implements' interfaces & methods] x]
@@ -99,44 +101,11 @@
       (f (first x)))
     (f x')
     (cond
-      (and (seq? x') (special-form? (first x'))) (with-lexical-context
-                                                   (handle-special-form (partial walk f) x'))
-      (coll? x')     (dorun (map (partial walk f) x')))))
+      (and (seq? x') (special-symbol? (first x')))
+      (with-lexical-context
+        (handle-special-form (partial walk! f) x')
+        (f ::end-of-binding-form))
 
-#_(walk prn '(defn foo [x] 1))
-
-#_(defn print-env [x]
-  (prn x)
-  (prn (locals))
-  (println))
-
-#_(walk print-env '(let [x 1] (let [x 2] x)))
-
-#_(walk print-env '(fn foo ([x] 5) ([x y] (+ x y))))
-
-#_(println "----")
-
-#_(walk identity '(fn moi [x] 1))
-
-
-#_(walk print-env '(def x "this is 5" 5))
-
-#_(walk print-env '(try
-                  1
-                  (+ 1 1)
-                  (catch RuntimeException r
-                    2
-                    (+ 2 2))
-                  (catch Exception e
-                    3
-                    (+ 3 3))
-                  (finally
-                    4
-                    (+ 4 4))))
-
-#_(walk print-env '(deftype Foo [x] Object (toString [this] "moi")))
-
-#_(walk print-env '(reify java.util.Date (toString [foo] "moi")))
-
-
+      (coll? x')
+      (dorun (map (partial walk! f) x')))))
 
